068e7b48fb635f146a5df2826c01ecbc8229ec22
luarakelly <lua.kellly@gmail.com>
2026-05-22 12:48:37 +0300

    mode side bar to compositions.

--- app/src/ui-lib/primitives/layout/index.ts
+++ app/src/ui-lib/primitives/layout/index.ts
@@ -1,9 +1,7 @@
 export * from "./Flex"
 export * from "./Grid"
 export * from "./Stack"
-export * from "./Sidebar"
+export * from "./Container"
 export * from "./Box"
 export * from "./Divider"
 export * from "./Spacer"
-
-function layoutBuilder() {}
