8f88b6f846e218be356db0496570a752d57fd244
luarakelly <lua.kellly@gmail.com>
2026-05-22 12:26:53 +0300

    refactor(ui-lib): remove TS design tokens and adopt headless layout system with only layout assistance focused CSS variables.

--- app/src/ui-lib/design-tokens/themes/index.ts
+++
@@ -1,9 +0,0 @@
-import { lightTheme } from "./light";
-import { darkTheme } from "./dark";
-
-export const themes = {
-  light: lightTheme,
-  dark: darkTheme,
-} as const;
-
-export type ThemeName = keyof typeof themes;
